88406e764470aa0d633881cbc9a41fb9d41b53c3
Saddam <eyarinsultana@gmail.com>
2026-05-26 09:40:15 +0600

    user service completed

--- src/api/services/auth.service.ts
+++ src/api/services/auth.service.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import type { RUser, User } from "../../types";
+import { sql } from "../../db";
 
 class AuthService {
   private async hashPassword(password: string): Promise<string> {
@@ -11,5 +13,87 @@ class AuthService {
   ): Promise<boolean> {
     return await bcrypt.compare(password, hash);
   }
-  createUser() {}
+  async createUser(user: RUser & { password: string }) {
+    const { name, email, password, role } = user;
+    const passwordHash = await this.hashPassword(password);
+    const result = await sql`
+    INSERT INTO users (name, email, password_hash, role) 
+    VALUES (${name}, ${email}, ${passwordHash}, COALESCE(${role}, 'contributor'))
+    RETURNING id, name, email, age, role, created_at, updated_at
+    `;
+    return result[0];
+  }
+  async validateUser(email: string, password: string) {
+    const result = await sql`
+      SELECT id, name, email, password_hash, role
+      FROM users
+      WHERE email = ${email}
+    `;
+
+    if (!result.length) {
+      return null;
+    }
+
+    const { password_hash, ...user } = result[0] as User;
+
+    const isValid = await this.comparePassword(password, password_hash);
+
+    return isValid ? user : null;
+  }
+  async getUsers() {
+    const result = await sql`
+      SELECT id, name, email, age, role
+      FROM users
+    `;
+    return result;
+  }
+
+  async getUserById(userId: string) {
+    const result = await sql`
+      SELECT id, name, email, age, role
+      FROM users
+      WHERE id = ${userId}
+    `;
+    return result[0] as RUser & { id: string };
+  }
+
+  async updateUser(
+    userId: string,
+    updates: Partial<RUser> & {
+      password?: string;
+    },
+  ) {
+    const { name, email, role, password } = updates;
+
+    let passwordHash: string | undefined;
+    if (password) {
+      passwordHash = await this.hashPassword(password);
+    }
+
+    const result = await sql`
+      UPDATE users
+      SET
+        name = COALESCE(${name}, name),
+        email = COALESCE(${email}, email),
+        role = COALESCE(${role}, role),
+        password_hash = COALESCE(${passwordHash}, password_hash),
+        updated_at = NOW()
+      WHERE id = ${userId}
+      RETURNING id, name, email, role, created_at, updated_at
+    `;
+    return result[0];
+  }
+
+  async deleteUser(userId: string) {
+    try {
+      await sql`
+        DELETE FROM users
+        WHERE id = ${userId}
+      `;
+      return true;
+    } catch {
+      return false;
+    }
+  }
 }
+export default new AuthService();
