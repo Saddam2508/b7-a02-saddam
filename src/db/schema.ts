39bbe71090c4f41242231e72964c9039cc0a7ffc
Saddam <eyarinsultana@gmail.com>
2026-05-26 11:31:31 +0600

    createIssueSchema added

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -13,3 +13,18 @@ export const createUserSchema = async () => {
     )
   `;
 };
+
+export const createIssueSchema = async () => {
+  await sql`
+    CREATE TABLE IF NOT EXISTS issues (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(150) NOT NULL,
+      description text  NOT NULL CHECK (char_length(description)>=20),
+      type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+        status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+      reporter_id INT NOT NULL,
+      created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+  `;
+};
